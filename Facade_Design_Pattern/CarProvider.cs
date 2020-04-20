a2a429f6e497713ff987bd2348017029d9132ee3
shashanks4 <49721667+shashanks4@users.noreply.github.com>
2020-04-20 20:28:49 +0530

    Add files via upload

---
+++ Facade_Design_Pattern/CarProvider.cs
@@ -0,0 +1,14 @@
+// using System;
+// using Facade_Design_Pattern;
+
+// public class CarProvider: ICar{
+//     public void CarModel(){
+//         Console.WriteLine("Setting CarModel as Benz");
+//     }
+//     public void CarEngine(){
+//         Console.WriteLine("Setting carEngine as Petrol");
+//     }
+//     public void CarBody(){
+//         Console.WriteLine("Setting carBody as BulletProof");
+//     }
+// }
---
+++ Facade_Design_Pattern/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace Facade_Design_Pattern
+{
+ 
+// Subclass
+    class CarModel
+    {
+        public void SetModel()
+        {
+            Console.WriteLine(" CarModel - SetModel");
+        }
+    }
+
+    class CarEngine
+    {
+        public void SetEngine()
+        {
+            Console.WriteLine(" CarEngine - SetEngine");
+        }
+    }
+
+    // The 'Subsystem ClassC' class
+    class CarBody
+    {
+        public void SetBody()
+        {
+            Console.WriteLine(" CarBody - SetBody");
+        }
+    }
+
+    // The 'Subsystem ClassD' class
+    class CarAccessories
+    {
+        public void SetAccessories()
+        {
+            Console.WriteLine(" CarAccessories - SetAccessories");
+        }
+    }
+
+    // The 'Facade' class
+    public class CarFacade
+    {
+        private readonly CarAccessories accessories;
+        private readonly CarBody body;
+        private readonly CarEngine engine;
+        private readonly CarModel model;
+
+        public CarFacade()
+        {
+            accessories = new CarAccessories();
+            body = new CarBody();
+            engine = new CarEngine();
+            model = new CarModel();
+        }
+
+        public void CreateCompleteCar()
+        {
+            Console.WriteLine("******** Creating a Car **********");
+            model.SetModel();
+            engine.SetEngine();
+            body.SetBody();
+            accessories.SetAccessories();
+
+           
+        }
+    }
+
+    // Facade pattern demo
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var facade = new CarFacade();
+
+            facade.CreateCompleteCar();
+
+            Console.ReadKey();
+        }
+    }
+}
+
